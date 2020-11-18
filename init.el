d1352d4d0c3dfd3d7a2aadb30408a39cf089a4f7
JavMatz <erick97am@gmail.com>
2020-11-17 23:38:53 -0600

    First commit. Work in progress

---
+++ init.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (tango-dark))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Custom key rebindings
+(global-set-key (kbd "C-c <right>") 'next-buffer)
+(global-set-key (kbd "C-c <left>") 'previous-buffer)
